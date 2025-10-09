685c56083aa4a06d4185e35d7440dc2aa5076999
Jarl Stephansson <jarl.stephansson@gmail.com>
2025-10-09 14:34:32 +0200

    created ds

--- BPM extraction/BPM.py
+++ BPM extraction/BPM.py
@@ -6,14 +6,14 @@ import librosa
 class BPM_extractor(): 
 
     def __init__(self, sr, novelty = "spectral", tempogram = "fourier", 
-                 novelty_w: float = 0.1, novelty_hop: float = 0.05,
-                 tempo_w: float = 0.1, tempo_hop: float = 0.05):
+                 novelty_w: float = 1.0, novelty_hop: float = 0.1,
+                 tempo_w: float = 20.0, tempo_hop: float = 1.0):
         self.sr = sr
         self.nfft = 8192
         self.novelty_window = (novelty_w, novelty_hop) # the window used in novelty calc IN SECONDS (window size, hop size)
         self.tempo_window = (tempo_w, tempo_hop)
-        self.bpm_max = 200
-        self.bpm_min = 60
+        self.bpm_max = 180
+        self.bpm_min = 100
 
         if novelty == "spectral": 
             self.novelty_f = self._spectral_novelty
@@ -62,13 +62,13 @@ class BPM_extractor():
         diff = mag[1:] - mag[:-1]
         diff[diff < 0.0] = 0.0
         novelty = diff.sum(axis=1)
-        m = novelty.max() 
+        m = np.max(novelty) 
         novelty = novelty / m # normalize
         return novelty
 
     def plot_novelty(self, sound, win_size = 0.1, hop = 0.05): 
         time_axis = np.linspace(0, len(sound) / self.sr, num=len(sound), endpoint=False)
-        novelty = self.novelty_f(sound, win_size, hop)
+        novelty = self.novelty_f(sound)
         # Create time axis for novelty (centered between frames)
         novelty_time = np.arange(len(novelty)) * hop + (win_size / 2)
 
@@ -95,12 +95,12 @@ class BPM_extractor():
         hop_size = int(self.tempo_window[1] * novelty_fs)
         window = np.hanning(win_len)
         frames = np.lib.stride_tricks.sliding_window_view(novelty, win_len)[::hop_size]
-        window = np.hanning(win_len)
         frames = frames * window[None, :]
 
         F = np.fft.rfft(frames, n=self.nfft, axis=1)  # FFT
         mag = np.abs(F)
-
+        
+        
         # --- build BPM axis from novelty “frequency” (cycles per second) ---
         freqs_hz = np.fft.rfftfreq(self.nfft, d=1.0/novelty_fs)  # periodicity in Hz of novelty
         bpm_axis = freqs_hz * 60.0
@@ -114,23 +114,84 @@ class BPM_extractor():
 
         # Optional normalization (per-timeframe) to emphasize relative peaks:
         # tempogram /= (tempogram.max(axis=1, keepdims=True) + 1e-10)
+        max_idx = np.argmax(tempogram, axis=1)
+        max_bpm = bpm_axis[max_idx]
+        print(f"Estimated global tempo: {np.mean(max_bpm):.1f} BPM")
 
+        return tempogram, bpm_axis, time_axis
+
+
+    def _autoCorrelation_tempogram(self, novelity):
+        novelty_fs = 1.0 / self.novelty_window[1]  # novelty frames per second
+
+        win_len = int(self.tempo_window[0] * novelty_fs)
+        hop_size = int(self.tempo_window[1] * novelty_fs)
+        window = np.hanning(win_len)
+        frames = np.lib.stride_tricks.sliding_window_view(novelity, win_len)[::hop_size]
+        frames = frames * window[None, :]
+        # now compute short-time autocorrelation function
+        F = np.fft.rfft(frames, n=self.nfft, axis=1)
+        acf_full = np.fft.irfft(np.abs(F)**2, n=self.nfft, axis=1)
+        acf = acf_full[:, :win_len] # Only non-negative lags 
+        
+        acf[:, 0] = 0.0 # this removes lag0 spike
+        acf = np.maximum(acf, 0.0)
+        lags = np.arange(win_len)                          # in novelty frames
+        # Avoid lag 0 (already zeroed); start from lag=1
+        freqs_hz = novelty_fs / np.maximum(lags, 1)            # periodicity (cycles/sec)
+        bpm_axis_all = freqs_hz * 60.0
+
+        keep = (bpm_axis_all >= self.bpm_min) & (bpm_axis_all <= self.bpm_max) & (lags >= 1 )
+
+        tempogram = acf[:, keep]
+        bpm_axis = bpm_axis_all[keep]
+
+        # Time (sec) at centers of analysis windows
+        time_axis = (np.arange(frames.shape[0]) * hop_size + 0.5 * win_len) / novelty_fs
+
+        avg_strength = tempogram.mean(axis=0)
+        bpm_est = bpm_axis[np.argmax(avg_strength)]
+        print(f"Estimated global tempo: {bpm_est:.1f} BPM")
+        
         return tempogram, bpm_axis, time_axis
 
 
 
 
+    def plot_tempogram(self, tempgram: np.ndarray, bpm_axis: np.ndarray, time_axeis: np.ndarray, overlay_max: bool = True):
 
-        
+        # Plot
+        fig, ax = plt.subplots(figsize=(10, 4))
+        extent = [time_axeis[0], time_axeis[-1], bpm_axis[0], bpm_axis[-1]]  # [xmin, xmax, ymin, ymax]
+        im = ax.imshow(tempgram.T, aspect='auto', origin='lower', extent=extent)
+        cbar = plt.colorbar(im, ax=ax, pad=0.01)
+        cbar.set_label("Strength")
+        if self.tempogram == self._fourier_tempogram: 
+            title = "fourier tempogram"
+        else: 
+            title = "Autocorrelation tempogram"
 
-    def _autoCorrelation_tempogram(self):
-        pass
+
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Tempo (BPM)")
+        ax.set_title(title)
+
+        # Optional: overlay per-frame max BPM curve
+        if overlay_max:
+            max_idx = np.argmax(tempgram, axis=1)              # (T,)
+            max_bpm = bpm_axis[max_idx]                        # (T,)
+            ax.plot(time_axeis, max_bpm, linewidth=1.5)
+
+        plt.tight_layout()
+        plt.show()
+
 
     def get_BPM(self, sound): 
-        pass
+        novelty = self.novelty_f(sound)
+        #self.plot_novelty(sound)
+        tempogram, bpm_axis, time_axis = self.tempogram(novelty)
+        self.plot_tempogram(tempogram, bpm_axis, time_axis, overlay_max=True)
+        
 
-
-
-
-    def _phase_novelty(self, sound): 
-            pass
+    #def _phase_novelty(self, novelity):  #TODO maybe try this novelity func aswell
+        
