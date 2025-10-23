c4dbbc6bb5d7d7ad658bfbd1ea595c8c62559bdb
Oskar Lövgren <oskarlov@kth.se>
2025-10-23 18:03:47 +0200

    adjusted template

--- src/utilities/predictKey.py
+++ src/utilities/predictKey.py
@@ -30,14 +30,18 @@ def predict_key(audio_path):
     #Krumhansl–Schmuckler templates
     kk_major = np.array([6.35,2.23,3.48,2.33,4.38,4.09,2.52,5.19,2.39,3.66,2.29,2.88], dtype=float)
     kk_minor = np.array([6.33,2.68,3.52,5.38,2.60,3.53,2.54,4.75,3.98,2.69,3.34,3.17], dtype=float)
-    kk_major = norm(kk_major)
-    kk_minor = norm(kk_minor)
+
+    #Adjusted templates
+    ak_major = np.array([6.5,2.0,3.48,2.30,4.5,4.0,2.5,5.19,2.39,3.68,2.29,3.40], dtype=float)
+    ak_minor = np.array([6.42,2.68,3.50,5.38,2.60,3.51,2.53,5.25,3.99,2.69,4.25,3.18], dtype=float)
+    ak_major = norm(ak_major)
+    ak_minor = norm(ak_minor)
 
     #rotate through all tonics, save the best correlation
     best_key, best_mode, best_corr = None, None, -np.inf
     for tonic in range(12):
-        maj = np.roll(kk_major, tonic)
-        minr = np.roll(kk_minor, tonic)
+        maj = np.roll(ak_major, tonic)
+        minr = np.roll(ak_minor, tonic)
 
         cmaj = np.dot(pcp, maj) / (np.linalg.norm(pcp)*np.linalg.norm(maj) + 1e-12)
         cmin = np.dot(pcp, minr) / (np.linalg.norm(pcp)*np.linalg.norm(minr) + 1e-12)
